a209960e284a2f34b55c39431d1a5dfff76c86bd
Tom Teck <tom.teck@gmail.com>
2023-11-09 07:24:55 +0100

    basic dashboard under construction

---
+++ app/dashboard/consultations/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConsultationsPage = () => {
+  return (
+    <div>ConsultationsPage</div>
+  )
+}
+
+export default ConsultationsPage
---
+++ app/ui/dashboard/sidebar/Sidebar.jsx
@@ -0,0 +1,112 @@
+import Image from "next/image";
+import MenuLink from "./menuLink/MenuLink";
+import styles from "./sidebar.module.css";
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: "Consultations",
+        path: "/dashboard/consultations",
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Revenue",
+        path: "/dashboard/revenue",
+        icon: <MdWork />,
+      },
+      {
+        title: "Reports",
+        path: "/dashboard/reports",
+        icon: <MdAnalytics />,
+      },
+      {
+        title: "Teams",
+        path: "/dashboard/teams",
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+      {
+        title: "Logout",
+        path: "/dashboard/teams",
+        icon: <MdLogout />,
+      },
+    ],
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50" />
+      </div>
+      <div className={styles.userDetail}>
+        <span className={styles.username}>John Doe</span>
+        <span className={styles.userTitle}>Administrator</span>
+      </div>
+        <ul className={styles.list}>
+          {menuItems.map((cat) => (
+            <li key={cat.title}>
+                <span className={styles.cat}>
+                {cat.title}
+                </span>
+                {cat.list.map((item) =>(
+                    <MenuLink key={item.title}item={item}/>
+                ))}
+                </li>
+          ))}
+        </ul>
+      
+    </div>
+  );
+};
+
+export default Sidebar;
